9d385a779a15a42f10f34f8a4dcd391723f70d90
Moshi <li.moshi95@gmail.com>
2022-06-09 19:39:40 -0400

    textarea value tracked

--- src/Components/StoryInput.tsx
+++ src/Components/StoryInput.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
@@ -9,14 +9,21 @@ import "./StoryInput.scss";
 
 const StoryInput = () => {
   const { fetching } = useSelector((store: RootStoreI) => store.dataReducer);
+  const [value, setValue] = useState("");
   const appDispatchAction = useAppDispatch();
   return (
     <div className="story--input--container">
-      <textarea className="story--input--textarea"></textarea>
+      <textarea
+        className="story--input--textarea"
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      ></textarea>
       {!fetching && (
         <button
           className="story--input--btn"
-          onClick={() => appDispatchAction(fetchData(""))}
+          onClick={() => appDispatchAction(fetchData(value))}
         >
           Test
         </button>
